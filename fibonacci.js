81ed5ff6d88d8cfe3a008f95d19e4c2b5fc931d3
Mirko1807 <cavolamirko@gmail.com>
2020-02-20 10:31:21 +0100

    prima versione

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,12 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+const fibonacci = (x, o, z) => {
+    let y = o
+    console.log(z)
+    o += z
+    z = y
+    if (z >= x) return z
+    z = fibonacci(x, o, z)
+}
+let z = fibonacci(30, 1, 0)
