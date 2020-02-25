76522ae3a3fa4f24c43345007aa2420b9d67ebc4
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-02-25 20:01:51 +0100

    Update deep.js

--- deep.js
+++ deep.js
@@ -1,3 +1,31 @@
 /**
 * Scrivere qui il codice dell'esercizio 4 del capitolo 4 di eloquentJS
 **/
+let d
+const deep = (x, y) => {
+    if ((x === null || y === null) && x === y) return true
+    else if (x === null || y === null) return false
+    if (x != null && y != null) {
+        let typex = typeof x
+        let typey = typeof y
+        console.log(typex, typey)
+        if (typex != typey) return false
+        if (typex === typey && typey != "string") {
+            let xs = JSON.stringify(x)
+            let ys = JSON.stringify(y)
+            return deep(xs, ys)
+        }
+        if (typex === typey && typex === "string") {
+            if (x === y) return true
+            else return false
+
+        }
+
+
+    }
+}
+let xa = true
+let ya = true
+d = deep(xa, ya)
+if (d) console.log("gli elementi sono uguali")
+else console.log("gli elementi non sono uguali")
