c34f85550d92d3ebbea2205f104ef9a1ba791f3a
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-02-25 20:04:11 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,16 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+const range = (x, y) => {
+    let arr = []
+    for (let i = 0; i < y; i++) arr[i] = x + i
+    return arr
+}
+const sum = (x) => {
+    let somma = 0
+    for (let i = 0; i < x.length; i++) somma = somma + x[i]
+    return somma
+}
+V = range(1, 10)
+console.log(V)
+console.log(sum(V))
